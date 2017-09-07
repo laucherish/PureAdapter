76f9fb57dc2a2569670016b71ec3278ab1f6ef5a
laucherish <laucherish@gmail.com>
2017-09-07 21:55:44 +0800

    Add Header Footer

--- library/src/main/java/com/laucherish/pureadapter/PureAdapter.java
+++ library/src/main/java/com/laucherish/pureadapter/PureAdapter.java
@@ -19,6 +19,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 /**
  * Created by laucherish on 2017/8/31.
  */
@@ -537,5 +540,232 @@ public abstract class PureAdapter<T> extends RecyclerView.Adapter<PureViewHolder
 
         void onLoadMoreRequested();
 
+    }
+
+    /**
+     * Append header to the rear of the mHeaderLayout.
+     *
+     * @param header
+     */
+    public int addHeaderView(View header) {
+        return addHeaderView(header, -1);
+    }
+
+    /**
+     * Add header view to mHeaderLayout and set header view position in mHeaderLayout.
+     * When index = -1 or index >= child count in mHeaderLayout,
+     * the effect of this method is the same as that of {@link #addHeaderView(View)}.
+     *
+     * @param header
+     * @param index  the position in mHeaderLayout of this header.
+     *               When index = -1 or index >= child count in mHeaderLayout,
+     *               the effect of this method is the same as that of {@link #addHeaderView(View)}.
+     */
+    public int addHeaderView(View header, int index) {
+        return addHeaderView(header, index, LinearLayout.VERTICAL);
+    }
+
+    /**
+     * @param header
+     * @param index
+     * @param orientation
+     */
+    public int addHeaderView(View header, int index, int orientation) {
+        if (mHeaderLayout == null) {
+            mHeaderLayout = new LinearLayout(header.getContext());
+            if (orientation == LinearLayout.VERTICAL) {
+                mHeaderLayout.setOrientation(LinearLayout.VERTICAL);
+                mHeaderLayout.setLayoutParams(new RecyclerView.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+            } else {
+                mHeaderLayout.setOrientation(LinearLayout.HORIZONTAL);
+                mHeaderLayout.setLayoutParams(new RecyclerView.LayoutParams(WRAP_CONTENT, MATCH_PARENT));
+            }
+        }
+        final int childCount = mHeaderLayout.getChildCount();
+        if (index < 0 || index > childCount) {
+            index = childCount;
+        }
+        mHeaderLayout.addView(header, index);
+        if (mHeaderLayout.getChildCount() == 1) {
+            int position = getHeaderViewPosition();
+            if (position != -1) {
+                notifyItemInserted(position);
+            }
+        }
+        return index;
+    }
+
+    public int setHeaderView(View header) {
+        return setHeaderView(header, 0, LinearLayout.VERTICAL);
+    }
+
+    public int setHeaderView(View header, int index) {
+        return setHeaderView(header, index, LinearLayout.VERTICAL);
+    }
+
+    public int setHeaderView(View header, int index, int orientation) {
+        if (mHeaderLayout == null || mHeaderLayout.getChildCount() <= index) {
+            return addHeaderView(header, index, orientation);
+        } else {
+            mHeaderLayout.removeViewAt(index);
+            mHeaderLayout.addView(header, index);
+            return index;
+        }
+    }
+
+    /**
+     * Append footer to the rear of the mFooterLayout.
+     *
+     * @param footer
+     */
+    public int addFooterView(View footer) {
+        return addFooterView(footer, -1, LinearLayout.VERTICAL);
+    }
+
+    public int addFooterView(View footer, int index) {
+        return addFooterView(footer, index, LinearLayout.VERTICAL);
+    }
+
+    /**
+     * Add footer view to mFooterLayout and set footer view position in mFooterLayout.
+     * When index = -1 or index >= child count in mFooterLayout,
+     * the effect of this method is the same as that of {@link #addFooterView(View)}.
+     *
+     * @param footer
+     * @param index  the position in mFooterLayout of this footer.
+     *               When index = -1 or index >= child count in mFooterLayout,
+     *               the effect of this method is the same as that of {@link #addFooterView(View)}.
+     */
+    public int addFooterView(View footer, int index, int orientation) {
+        if (mFooterLayout == null) {
+            mFooterLayout = new LinearLayout(footer.getContext());
+            if (orientation == LinearLayout.VERTICAL) {
+                mFooterLayout.setOrientation(LinearLayout.VERTICAL);
+                mFooterLayout.setLayoutParams(new RecyclerView.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+            } else {
+                mFooterLayout.setOrientation(LinearLayout.HORIZONTAL);
+                mFooterLayout.setLayoutParams(new RecyclerView.LayoutParams(WRAP_CONTENT, MATCH_PARENT));
+            }
+        }
+        final int childCount = mFooterLayout.getChildCount();
+        if (index < 0 || index > childCount) {
+            index = childCount;
+        }
+        mFooterLayout.addView(footer, index);
+        if (mFooterLayout.getChildCount() == 1) {
+            int position = getFooterViewPosition();
+            if (position != -1) {
+                notifyItemInserted(position);
+            }
+        }
+        return index;
+    }
+
+    public int setFooterView(View header) {
+        return setFooterView(header, 0, LinearLayout.VERTICAL);
+    }
+
+    public int setFooterView(View header, int index) {
+        return setFooterView(header, index, LinearLayout.VERTICAL);
+    }
+
+    public int setFooterView(View header, int index, int orientation) {
+        if (mFooterLayout == null || mFooterLayout.getChildCount() <= index) {
+            return addFooterView(header, index, orientation);
+        } else {
+            mFooterLayout.removeViewAt(index);
+            mFooterLayout.addView(header, index);
+            return index;
+        }
+    }
+
+    /**
+     * remove header view from mHeaderLayout.
+     * When the child count of mHeaderLayout is 0, mHeaderLayout will be set to null.
+     *
+     * @param header
+     */
+    public void removeHeaderView(View header) {
+        if (getHeaderLayoutCount() == 0) return;
+
+        mHeaderLayout.removeView(header);
+        if (mHeaderLayout.getChildCount() == 0) {
+            int position = getHeaderViewPosition();
+            if (position != -1) {
+                notifyItemRemoved(position);
+            }
+        }
+    }
+
+    /**
+     * remove footer view from mFooterLayout,
+     * When the child count of mFooterLayout is 0, mFooterLayout will be set to null.
+     *
+     * @param footer
+     */
+    public void removeFooterView(View footer) {
+        if (getFooterLayoutCount() == 0) return;
+
+        mFooterLayout.removeView(footer);
+        if (mFooterLayout.getChildCount() == 0) {
+            int position = getFooterViewPosition();
+            if (position != -1) {
+                notifyItemRemoved(position);
+            }
+        }
+    }
+
+    /**
+     * remove all header view from mHeaderLayout and set null to mHeaderLayout
+     */
+    public void removeAllHeaderView() {
+        if (getHeaderLayoutCount() == 0) return;
+
+        mHeaderLayout.removeAllViews();
+        int position = getHeaderViewPosition();
+        if (position != -1) {
+            notifyItemRemoved(position);
+        }
+    }
+
+    /**
+     * remove all footer view from mFooterLayout and set null to mFooterLayout
+     */
+    public void removeAllFooterView() {
+        if (getFooterLayoutCount() == 0) return;
+
+        mFooterLayout.removeAllViews();
+        int position = getFooterViewPosition();
+        if (position != -1) {
+            notifyItemRemoved(position);
+        }
+    }
+
+    private int getHeaderViewPosition() {
+        //Return to header view notify position
+        if (getEmptyViewCount() == 1) {
+            if (mHeadAndEmptyEnable) {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+        return -1;
+    }
+
+    private int getFooterViewPosition() {
+        //Return to footer view notify position
+        if (getEmptyViewCount() == 1) {
+            int position = 1;
+            if (mHeadAndEmptyEnable && getHeaderLayoutCount() != 0) {
+                position++;
+            }
+            if (mFootAndEmptyEnable) {
+                return position;
+            }
+        } else {
+            return getHeaderLayoutCount() + mData.size();
+        }
+        return -1;
     }
 }
