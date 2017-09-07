bc966a5e7be5c4e802588f7a24ee2bd6ae6dd267
laucherish <laucherish@gmail.com>
2017-09-07 23:09:25 +0800

    Add Header Footer

--- app/src/main/java/com/laucherish/pureadapterapp/MainActivity.java
+++ app/src/main/java/com/laucherish/pureadapterapp/MainActivity.java
@@ -43,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
         mRowNumber = 20;
         myAdapter = new MyAdapter();
         View headerView = LayoutInflater.from(this).inflate(R.layout.view_my_header, null);
+        View footerView = LayoutInflater.from(this).inflate(R.layout.view_my_footer, null);
         myAdapter.addHeaderView(headerView);
+        myAdapter.addFooterView(footerView);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setAdapter(myAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
