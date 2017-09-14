409d6efddb34d154fe816c4f12b48e83d8bc4068
laucherish <laucherish@gmail.com>
2017-09-14 10:25:06 +0800

    Add Header Footer

--- app/src/main/java/com/laucherish/pureadapterapp/MyAdapter.java
+++ app/src/main/java/com/laucherish/pureadapterapp/MyAdapter.java
@@ -16,5 +16,6 @@ public class MyAdapter extends PureAdapter<String> {
     @Override
     protected void convert(PureViewHolder helper, String item) {
         helper.setText(R.id.tv_title, item);
+
     }
 }
